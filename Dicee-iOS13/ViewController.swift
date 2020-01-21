717bf554b4ec87889381a3e8613a394e6de54d94
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-21 13:55:36 +0700

    add randomize dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,28 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImage1: UIImageView!
     @IBOutlet weak var diceImage2: UIImageView!
     
-    var leftDice = 1
-    var rightDice = 5
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //MARK: Dice image 1
-        diceImage1.image = #imageLiteral(resourceName: "DiceSix")
+//        diceImage1.image = #imageLiteral(resourceName: "DiceSix")
         
         //MARK: Dice image 2
-        diceImage2.image = #imageLiteral(resourceName: "DiceTwo")
+//        diceImage2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
     @IBAction func rollButtonClicked(_ sender: UIButton) {
         
-        diceImage1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [leftDice]
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        leftDice = leftDice + 1
-        
-        diceImage2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [rightDice]
-        
-        rightDice = rightDice - 1
+        diceImage1.image = diceArray.randomElement()
+        diceImage2.image = diceArray.randomElement()
         
         
     }
